10f707bdbf6e0c6e67fc91c95673ca3210b88120
Yan Gianini <yangianini@gmail.com>
2021-06-14 15:01:51 -0300

    classe protudo adicionado

---
+++ application/model/entity/produto.py
@@ -0,0 +1,49 @@
+class Produto():
+    def __init__(self, id, nome, imagens, preco, parcela, valParcela):
+        self.__id = id
+        self.__nome = nome
+        self.__imagens = imagens
+        self.__preco = preco
+        self.__parcela = parcela
+        self.__valParcela = valParcela
+
+def get_id(self):
+    return self.__id
+
+def set_id(self, id):
+    self.__id = id
+
+
+def get_nome(self):
+    return self.__nome
+
+def set_nome(self, nome):
+    self.__nome = nome
+
+
+def get_imagens(self):
+    return self.__imagens
+
+def add_imagem(self, img):
+    self.__imagem.append(img)
+
+
+def get_preco(self):
+    return self.__preco
+
+def set_preco(self, preco):
+    self.__preco = preco
+
+
+def get_parcela(self):
+    return self.__parcela
+
+def set_parcela(self, parcela):
+    self.__parcela = parcela
+
+
+def get_valParcela(self):
+    return self.__valParcela
+
+def set_valParcela(self, valParcela):
+    self.__valParcela = valParcela
